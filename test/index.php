47b4ce8733727c41ee0a957deeef8bf35f7525d6
debasis778 <debasis778@gmail.com>
2018-05-05 19:05:25 +0530

    tbabysuit web version 1.0

---
+++ test/index.php
@@ -0,0 +1,210 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>T-BabySuit</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/united/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/4/united/bootstrap.css">
+    
+    <link rel="stylesheet" type="" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  </head>
+  <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">T-BabySuit</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarColor02" style="margin-left: 650px;">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link  " style="color:goldenrod;"  href="#home">HOME <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link " style="color:goldenrod;"  href="#features">FEATURES</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link " style="color:goldenrod;"  href="#pricing">PRICING</a>
+                    </li>
+                    <li class="nav-item"> 
+                      <a class="nav-link " style="color:goldenrod;" href="#aboutus">ABOUT US</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  " style="color:goldenrod;"  href="#contactus">CONTACT US</a>
+                    </li>
+                  </ul>
+                  
+                </div>
+        </nav>
+        <div class="container-fluid" id="home">
+            <div class="row" id="header">
+                <div class="col-md-1 col-lg-1">
+
+                </div>
+                <div class="col-md-10 col-lg-10">
+                    <div class="card" id="card">
+                        <div class="card-header " style="background:#772953;font-weight:bold;color:goldenrod;">
+                            <h4 align="center">What is T-BabySuit<i class="fa fa-question-circle" aria-hidden="true"></i></h4>
+                        </div>
+                        <div class="card-body">
+                                <iframe class="embed-responsive embed" width="560" height="315" src="https://www.youtube.com/embed/4-BTxXm8KSg" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen>
+                                </iframe>
+                        </div>
+                        <div class="card-footer"  style="background:#772953;font-weight:bold;color:goldenrod;">
+                            <p>Parents are happy when they are gifted with a baby and also concerned about health of a baby and especially if a baby is born as preterm because these babies are susceptible to breathing problems like Hypoxia and Apnea and also few babies do suffer from fever and diarrohea or constipation. These are the critical condition that makes every parent to worry about the health of a baby.</p>
+                            <p>T-BabySuit is a smart baby suit that will help the parents in monitoring the health of a baby right from the home.</p>  
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-md-1 col-lg-1">
+
+                </div>
+            </div>
+
+        
+
+        </div>
+
+        <div class="container-fluid" id="features">
+            <div class="row" id="header">
+                <div class="col-md-1 col-lg-1">
+
+                </div>
+                <div class="col-md-10 col-lg-10">
+                    <div class="card" id="card">
+                        <div class="card-header " style="background:#772953;font-weight:bold;color:goldenrod;">
+                            <h3 align="center">Our Best Fetures</h3> 
+                        </div>
+                        <div class="card-body">
+                                <blockquote class="blockquote text-center">
+                                        <p class="mb-0">To keep the body in good health is a duty... otherwise we shall not be able to keep our mind strong and clear.</p>
+                                        <footer class="blockquote-footer"> <cite title="Source Title">Buddha</cite></footer>
+                                      </blockquote>
+                                
+                        </div>
+                        <div class="card-footer"  style="background:#772953;font-weight:bold;color:goldenrod;">
+                            <p><i class="fa fa-check" aria-hidden="true"></i> It will measure baby’s body temperature. </p>
+                            <p><i class="fa fa-check" aria-hidden="true"></i>It will measure baby's pulse rate for any breathing problem.</p>
+                            <p><i class="fa fa-check" aria-hidden="true"></i>It will alert the parents if a baby cries.</p>
+                            <p><i class="fa fa-check" aria-hidden="true"></i>It will alert the parents if a baby urinates.</p>  
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-md-1 col-lg-1">
+
+                </div>
+            </div>
+
+        
+
+        </div>
+
+        <!--//PRICING-->
+        <div class="container-fluid" id="pricing">
+                <div class="row" id="header">
+                    <div class="col-md-1 col-lg-1">
+    
+                    </div>
+                    <div class="col-md-10 col-lg-10">
+                        <div class="card" id="card">
+                            <div class="card-header " style="background:#772953;font-weight:bold;color:goldenrod;">
+                                <h3 align="center">T-BabySuit Price</h3> 
+                            </div>
+                            <div class="card-body">
+                                    <blockquote class="blockquote text-center">
+                                            <p class="mb-0">Good health is not something we can buy. However, it can be an extremely valuable savings account.</p>
+                                            <footer class="blockquote-footer"> <cite title="Source Title">Anne Wilson Schaef</cite></footer>
+                                          </blockquote>
+                                    
+                            </div>
+                            <div class="card-footer"  style="background:#772953;font-weight:bold;color:goldenrod;">
+                                
+                            </div>
+                        </div>
+    
+                    </div>
+                    <div class="col-md-1 col-lg-1">
+    
+                    </div>
+                </div>
+    
+            
+    
+            </div>
+            <!--advantages-->
+            <div class="container-fluid" id="pricing">
+                    <div class="row" id="header">
+                        <div class="col-md-1 col-lg-1">
+        
+                        </div>
+                        <div class="col-md-10 col-lg-10">
+                            <div class="card" id="card">
+                                <div class="card-header " style="background:#772953;font-weight:bold;color:goldenrod;">
+                                    <h3 align="center">Advantages</h3> 
+                                </div>
+                                <div class="card-body">
+                                        <blockquote class="blockquote text-center">
+                                                <p class="mb-0">Calm mind brings inner strength and self-confidence, so that's very important for good health.</p>
+                                                <footer class="blockquote-footer"> <cite title="Source Title">Dalai Lama</cite></footer>
+                                              </blockquote>
+                                        
+                                        
+                                </div>
+                                <div class="card-footer"  style="background:#772953;font-weight:bold;color:goldenrod;">
+                                    
+                                </div>
+                            </div>
+        
+                        </div>
+                        <div class="col-md-1 col-lg-1">
+        
+                        </div>
+                    </div>
+        
+                
+        
+                </div>
+                <!-- current pictures and success-->
+                <div></div>
+                <!--like and dislike-->
+                <div></div>
+                <!--about us-->
+                <!--contact us-->
+
+
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+<div style="width:207px; height:33px; position:absolute; z-index:999; top: 4px; left: 400px;" hidden> 
+<?php 
+$handle = fopen("counter.txt", "r"); 
+if(!$handle){ 
+    echo "could not open the file"; 
+    } 
+    else { 
+        $counter=(int )fread($handle,20); 
+        fclose($handle); 
+        $counter++; 
+        echo" <strong>visitor no ". $counter . " </strong > " ; 
+        $handle= fopen("counter.txt", "w" ) ; 
+        fwrite($handle,$counter) ; 
+        fclose ($handle) ; 
+        } 
+        ?> 
+</div>
+   
+  </body>
+</html>
